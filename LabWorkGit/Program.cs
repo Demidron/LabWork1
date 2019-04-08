e011a1040f9d10fea1f1bd2f1c81653d82ceab49
Demidron <andrew.demidyuk@gmail.com>
2019-04-08 10:55:28 +0300

    Create DB

--- LabWorkGit/Program.cs
+++ LabWorkGit/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabWorkGit.DbLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace LabWorkGit
     {
         static void Main(string[] args)
         {
+            UserContext db = new UserContext();
+            db.Users.AddRange(DataManager.users);
+            db.SaveChanges();
+            db.Roles.AddRange(DataManager.roles);
+            db.SaveChanges();
+            db.UserInRoles.AddRange(DataManager.userinroles);
+            db.SaveChanges();
         }
     }
 }
