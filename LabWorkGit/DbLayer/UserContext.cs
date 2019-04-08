d989c86d98f23dfa025052cbf38ca24cfe23abdd
Demidron <andrew.demidyuk@gmail.com>
2019-04-08 10:54:40 +0300

    Add connection strings

---
+++ LabWorkGit/DbLayer/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWorkGit.DbLayer
+{
+    public class UserContext : DbContext
+    {
+        public  UserContext() : base("DbConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> UserInRoles { get; set; }
+
+    }
+}
